4de8afa17b1dac4e92527601dccc709d7380f0bd
Uwila <uwila@runbox.com>
2020-02-25 19:44:04 +0100

    Put code for vertex generation in subroutines

--- main.c
+++ main.c
@@ -124,6 +124,94 @@ static void initialize_glut_callbacks()
     glutDisplayFunc(render_scene_cb);
 }
 
+static void add_cell_vertices(
+     int x,
+     int y,
+     float width_interval,
+     float height_interval,
+     vector3f *vertices,
+     int *i
+    ) {
+    vertices[(*i)++] = create_vector3f(-1.0f + width_interval * 2 * x,
+                                       -1.0f + height_interval * 2 * y,
+                                       0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
+                                       -1.0f + height_interval * 2 * y,
+                                       0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
+                                       -1.0f + ((y * 2) + 1) * height_interval,
+                                       0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
+                                       -1.0f + ((y * 2) + 1) * height_interval,
+                                       0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + width_interval * 2 * x,
+                                       -1.0f + ((y * 2) + 1) * height_interval,
+                                       0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + width_interval * 2 * x,
+                                       -1.0f + height_interval * 2 * y,
+                                       0.0f);
+}
+
+// Adds the vertices for a vertical connection on top of the cell at (x,y)
+static void add_vertical_connection_vertices(
+    int x,
+    int y,
+    float width_interval,
+    float height_interval,
+    vector3f *vertices,
+    int *i
+    ) {
+    float width_interval_third = width_interval / 3;
+    vertices[(*i)++] = create_vector3f(-1.0f + (x * 2) * width_interval + width_interval_third,
+                                       -1.0f + ((y * 2) + 1) * height_interval,
+                                       0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval - width_interval_third,
+                                       -1.0f + ((y * 2) + 1) * height_interval,
+                                       0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval - width_interval_third,
+                                       -1.0f + ((y + 1) * 2) * height_interval,
+                                       0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval - width_interval_third,
+                                       -1.0f + ((y + 1) * 2) * height_interval,
+                                       0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + (x * 2) * width_interval + width_interval_third,
+                                       -1.0f + ((y + 1) * 2) * height_interval,
+                                       0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + (x * 2) * width_interval + width_interval_third,
+                                       -1.0f + ((y * 2) + 1) * height_interval,
+                                       0.0f);
+}
+
+// Adds the vertices for a horizontal connection to the right of the cell at (x,y)
+static void add_horizontal_connection_vertices(
+    int x,
+    int y,
+    float width_interval,
+    float height_interval,
+    vector3f *vertices,
+    int *i
+    ) {
+    float height_interval_third = height_interval / 3;
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
+                                    -1.0f + height_interval * 2 * y + height_interval_third,
+                                    0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x + 1) * 2) * width_interval,
+                                    -1.0f + height_interval * 2 * y + height_interval_third,
+                                    0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x + 1) * 2) * width_interval,
+                                    -1.0f + ((y * 2) + 1) * height_interval - height_interval_third,
+                                    0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x + 1) * 2) * width_interval,
+                                    -1.0f + ((y * 2) + 1) * height_interval - height_interval_third,
+                                    0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
+                                    -1.0f + ((y * 2) + 1) * height_interval - height_interval_third,
+                                    0.0f);
+    vertices[(*i)++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
+                                    -1.0f + height_interval * 2 * y + height_interval_third,
+                                    0.0f);
+}
+
 static void create_vertex_buffer()
 {
     int cell_count = width * height;
@@ -140,74 +228,17 @@ static void create_vertex_buffer()
 
     float width_interval = 2.0 / (width * 2.0 - 1.0);
     float height_interval = 2.0 / (height * 2.0 - 1.0);
-    float width_interval_third = width_interval / 3;
-    float height_interval_third = height_interval / 3;
 
     int i = 0;
     for (int x = 0; x < width; x++) {
         for (int y = 0; y < height; y++) {
-            vertices[i++] = create_vector3f(-1.0f + width_interval * 2 * x,
-                                            -1.0f + height_interval * 2 * y,
-                                            0.0f);
-            vertices[i++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
-                                            -1.0f + height_interval * 2 * y,
-                                            0.0f);
-            vertices[i++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
-                                            -1.0f + ((y * 2) + 1) * height_interval,
-                                            0.0f);
-            vertices[i++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
-                                            -1.0f + ((y * 2) + 1) * height_interval,
-                                            0.0f);
-            vertices[i++] = create_vector3f(-1.0f + width_interval * 2 * x,
-                                            -1.0f + ((y * 2) + 1) * height_interval,
-                                            0.0f);
-            vertices[i++] = create_vector3f(-1.0f + width_interval * 2 * x,
-                                            -1.0f + height_interval * 2 * y,
-                                            0.0f);
+            add_cell_vertices(x, y, width_interval, height_interval, vertices, &i);
             for (int n = 0; n < cells[x][y]->neighbour_count; n++) {
                 cell_t* neighbour = cells[x][y]->neighbours[n];
                 if (neighbour->x == x + 1) {
-                    vertices[i++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
-                                                    -1.0f + height_interval * 2 * y + height_interval_third,
-                                                    0.0f);
-                    vertices[i++] = create_vector3f(-1.0f + ((x + 1) * 2) * width_interval,
-                                                    -1.0f + height_interval * 2 * y + height_interval_third,
-                                                    0.0f);
-                    vertices[i++] = create_vector3f(-1.0f + ((x + 1) * 2) * width_interval,
-                                                    -1.0f + ((y * 2) + 1) * height_interval - height_interval_third,
-                                                    0.0f);
-                    vertices[i++] = create_vector3f(-1.0f + ((x + 1) * 2) * width_interval,
-                                                    -1.0f + ((y * 2) + 1) * height_interval - height_interval_third,
-                                                    0.0f);
-
-                    vertices[i++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
-                                                    -1.0f + ((y * 2) + 1) * height_interval - height_interval_third,
-                                                    0.0f);
-                    vertices[i++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval,
-                                                    -1.0f + height_interval * 2 * y + height_interval_third,
-                                                    0.0f);
-
-
-
+                    add_horizontal_connection_vertices(x, y, width_interval, height_interval, vertices, &i);
                 } else if (neighbour->y == y + 1) {
-                    vertices[i++] = create_vector3f(-1.0f + (x * 2) * width_interval + width_interval_third,
-                                                    -1.0f + ((y * 2) + 1) * height_interval,
-                                                    0.0f);
-                    vertices[i++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval - width_interval_third,
-                                                    -1.0f + ((y * 2) + 1) * height_interval,
-                                                    0.0f);
-                    vertices[i++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval - width_interval_third,
-                                                    -1.0f + ((y + 1) * 2) * height_interval,
-                                                    0.0f);
-                    vertices[i++] = create_vector3f(-1.0f + ((x * 2) + 1) * width_interval - width_interval_third,
-                                                    -1.0f + ((y + 1) * 2) * height_interval,
-                                                    0.0f);
-                    vertices[i++] = create_vector3f(-1.0f + (x * 2) * width_interval + width_interval_third,
-                                                    -1.0f + ((y + 1) * 2) * height_interval,
-                                                    0.0f);
-                    vertices[i++] = create_vector3f(-1.0f + (x * 2) * width_interval + width_interval_third,
-                                                    -1.0f + ((y * 2) + 1) * height_interval,
-                                                    0.0f);
+                    add_vertical_connection_vertices(x, y, width_interval, height_interval, vertices, &i);
                 }
             }
         }
