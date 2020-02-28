0a88faddace61c85ce8780cbc65c2c21117aab69
Uwila <uwila@runbox.com>
2020-02-28 22:26:18 +0100

    Remove outdated comments

--- main.c
+++ main.c
@@ -67,42 +67,7 @@ int main(int argc, char *argv[]) {
 
     pthread_t thread_id;
     pthread_create(&thread_id, NULL, do_networking, NULL);
-    // Child will handle networking, while parent handles graphics
-    /* pid_t child_pid = fork(); */
-    /* if (child_pid == 0) { */
-    /*     // Connect to client */
-    /*     int sockfd; */
-    /*     int conn_fd = connect_to_client(&sockfd); */
-
-    /*     struct msg message; */
-    /*     int nbytes; */
-
-    /*     // Spy stub code for communication with client */
-    /*     while((nbytes = read(conn_fd, &message, sizeof(message))) > 0) { */
-    /*         if (nbytes > 0) { */
-    /*             printf("(%i,%i)\n", message.x, message.y); */
-    /*             cells[message.x][message.y]->visited = 1; */
-    /*             for (int i = 0; i < cells[message.x][message.y]->neighbour_count; i++) { */
-    /*                 struct msg response = { */
-    /*                                        cells[message.x][message.y]->neighbours[i]->x, */
-    /*                                        cells[message.x][message.y]->neighbours[i]->y */
-    /*                 }; */
-    /*                 send(conn_fd, &response, sizeof(response), 0); */
-    /*             } */
-    /*         } */
-    /*     } */
-
-    /*     // Print the error if we encounter one */
-    /*     if (nbytes < 0) { */
-    /*         printf("%i bytes\n", nbytes); */
-    /*         printf("%s\n",strerror(errno)); */
-    /*     } */
-
-    /*     // Close when we're done */
-    /*     close(sockfd); */
-    /*     printf("Socket closed\n"); */
-    /* } else { */
-        // Initialize GLFW Window
+    // Initialize GLFW Window
     if (!glfwInit()) {
         printf("GLFW failed to init\n");
         return -1;
@@ -134,10 +99,6 @@ int main(int argc, char *argv[]) {
         render_scene_cb(window, width, height, connections);
         glfwPollEvents();
     }
-
-    // Make sure our child is dead (inter-process infanticide is no joke!)
-    /* kill(child_pid, SIGTERM); */
-    /* } */
 
     return 0;
 }
