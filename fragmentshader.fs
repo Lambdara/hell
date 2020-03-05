e47968cd7a94d79027da9d76af85aee568dfdb5e
Uwila <uwila@runbox.com>
2020-03-05 18:41:01 +0100

    Use light distance in lighting computation

--- fragmentshader.fs
+++ fragmentshader.fs
@@ -7,13 +7,13 @@ out vec3 pixel_color;
 void main() {
     vec3 normal = vec3(0.0, 0.0, 1.0);
 
-    vec3 light1_position = vec3(-1.0, -1.0, 0.3);
-    vec3 light1_direction = normalize(light1_position - fragment_position);
-    float light1 = max(dot(normal, light1_direction), 0.0);
+    vec3 light1_position = vec3(-1.0, -1.0, 0.5);
+    vec3 light1_diff = light1_position - fragment_position;
+    float light1 = max(dot(normal, normalize(light1_diff)), 0.0)/length(light1_diff);
 
-    vec3 light2_position = vec3(1.0, 1.0, 0.3);
-    vec3 light2_direction = normalize(light2_position - fragment_position);
-    float light2 = max(dot(normal, light2_direction), 0.0);
+    vec3 light2_position = vec3(1.0, 1.0, 0.5);
+    vec3 light2_diff = light2_position - fragment_position;
+    float light2 = max(dot(normal, normalize(light2_diff)), 0.0) / length(light2_diff);
 
     float light = (light1 + light2 + 0.6)/3.0;
 
